d1ae9e9395240aebd129bfe87aebb1737c5de664
xuchanglong <wwwitworld@sina.com>
2019-08-30 14:54:22 +0800

    Add comment to noncopyable.h .

--- _include/base/noncopyable.h
+++ _include/base/noncopyable.h
@@ -1,11 +1,17 @@
-#ifndef XMOON_MISC_NONCOPYABLE_
-#define XMOON_MISC_NONCOPYABLE_
+#ifndef XMOON_MISC_NONCOPYABLE_H_
+#define XMOON_MISC_NONCOPYABLE_H_
 
 class NonCopyable
 {
 public:
+    /**
+     * 该类禁止生成默认的拷贝构造函数和赋值构造函数。
+    */
     NonCopyable(const NonCopyable &kObj) = delete;
     NonCopyable &operator=(const NonCopyable &kObj) = delete;
+/**
+ * TODO：需要确定这里使用 protected 的原因。
+*/
 protected:
     NonCopyable() = default;
     ~NonCopyable() = default;
