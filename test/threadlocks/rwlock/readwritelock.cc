5d6fb5a68c1b0a1361553632b05fa21f0e215b1f
xuchanglong <wwwitworld@sina.com>
2019-09-18 10:01:52 +0800

    初步完成读写锁的实例代码。

--- test/threadlocks/rwlock/readwritelock.cc
+++ test/threadlocks/rwlock/readwritelock.cc
@@ -1,169 +1,139 @@
-
 /*****************************************************************************************
  * @function    使用读写锁实现4个线程读写一段内存的实例。
  * @notice      在任意时刻，如果有一个线程在写数据，将阻塞所有其他线程的任何操作。
  * @author      xuchanglong
  * @time        2019-09-17  
  *****************************************************************************************/
-#include <errno.h>
 #include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <bits/pthreadtypes.h>
-#include <unistd.h>
-#include <string.h>
+#include <iostream>
 
-static pthread_rwlock_t rwlock; //读写锁对象
+void *thread_func_read_one(void *arg);
+void *thread_func_read_two(void *arg);
+void *thread_func_write_one(void *arg);
+void *thread_func_write_two(void *arg);
 
-#define WORKSIZE 1024
-char work_area[WORKSIZE];
-int time_to_exit;
+using threadfunc = void *(*)(void *arg);
 
-void *thread_function_read_o(void *arg);  //读线程1
-void *thread_function_read_t(void *arg);  //读线程2
-void *thread_function_write_o(void *arg); //写线程1
-void *thread_function_write_t(void *arg); //写线程2
+static pthread_rwlock_t rwlock;
+static char wordarea[1024];
+static bool isexitall = false;
 
-int main(int argc, char *argv[])
+static const threadfunc threadfuncsum[] =
+    {
+        thread_func_read_one,
+        thread_func_read_two,
+        thread_func_write_one,
+        thread_func_write_two,
+};
+
+#define threadsum (sizeof(threadfuncsum) / sizeof(threadfunc))
+
+int main()
 {
-    int res;
-    pthread_t a_thread, b_thread, c_thread, d_thread;
-    void *thread_result;
+    int res = 0;
+    pthread_t thread_t_sum[threadsum];
 
-    res = pthread_rwlock_init(&rwlock, nullptr); //初始化读写锁
+    /**
+     * （1）初始化读写锁。
+    */
+    res = pthread_rwlock_init(&rwlock, nullptr);
     if (res != 0)
     {
-        perror("rwlock initialization failed");
-        exit(EXIT_FAILURE);
-    }
-    res = pthread_create(&a_thread, NULL, thread_function_read_o, NULL); //create new thread创建线程
-    if (res != 0)
-    {
-        perror("Thread creation failed");
-        exit(EXIT_FAILURE);
+        std::cout << "rwlock initialization failed." << std::endl;
+        return 1;
     }
 
-    res = pthread_create(&b_thread, NULL, thread_function_read_t, NULL); //create new thread
-    if (res != 0)
+    /**
+     * （2）开启所有线程。
+    */
+    for (size_t i = 0; i < threadsum; i++)
     {
-        perror("Thread creation failed");
-        exit(EXIT_FAILURE);
-    }
-    res = pthread_create(&c_thread, NULL, thread_function_write_o, NULL); //create new thread
-    if (res != 0)
-    {
-        perror("Thread creation failed");
-        exit(EXIT_FAILURE);
-    }
-    res = pthread_create(&d_thread, NULL, thread_function_write_t, NULL); //create new thread
-    if (res != 0)
-    {
-        perror("Thread creation failed");
-        exit(EXIT_FAILURE);
+        res = pthread_create(&thread_t_sum[i], nullptr, threadfuncsum[i], nullptr);
+        if (res != 0)
+        {
+            std::cout << "thread creation failed." << std::endl;
+            return 2;
+        }
     }
 
-    res = pthread_join(a_thread, &thread_result); //等待a_thread线程结束
-    if (res != 0)
+    /**
+     * （3）等待所有线程的退出。
+    */
+    for (size_t i = 0; i < threadsum; i++)
     {
-        perror("Thread join failed");
-        exit(EXIT_FAILURE);
-    }
-    res = pthread_join(b_thread, &thread_result);
-    if (res != 0)
-    {
-        perror("Thread join failed");
-        exit(EXIT_FAILURE);
-    }
-    res = pthread_join(c_thread, &thread_result);
-    if (res != 0)
-    {
-        perror("Thread join failed");
-        exit(EXIT_FAILURE);
-    }
-    res = pthread_join(d_thread, &thread_result);
-    if (res != 0)
-    {
-        perror("Thread join failed");
-        exit(EXIT_FAILURE);
+        res = pthread_join(thread_t_sum[i], nullptr);
+        if (res != 0)
+        {
+            std::cout << "create join failed." << std::endl;
+            return 3;
+        }
     }
 
-    pthread_rwlock_destroy(&rwlock); //销毁读写锁
-    exit(EXIT_SUCCESS);
+    /**
+     * （4）销毁读写锁。
+    */
+    pthread_rwlock_destroy(&rwlock);
+    return 0;
 }
 
-void *thread_function_read_o(void *arg)
+void *thread_func_read_one(void *arg)
 {
-    printf("thread read one try to get lock\n");
-
-    pthread_rwlock_rdlock(&rwlock); //获取读取锁
-    while (strncmp("end", work_area, 3) != 0)
+    std::cout << "read thread one start run." << std::endl;
+    while (strncmp("end", workarea, 3) != 0)
     {
-        printf("this is thread read one.");
-        printf("the characters is %s \n", work_area);
+        pthread_rwlock_lock(&rwlock);
+        std::cout << "read thread one get rwlock.";
+        std::cout << "workarea is :" << workarea << std::endl;
         pthread_rwlock_unlock(&rwlock);
         sleep(2);
-        pthread_rwlock_rdlock(&rwlock);
-        while (work_area[0] == '\0')
-        {
-            pthread_rwlock_unlock(&rwlock);
-            sleep(2);
-            pthread_rwlock_rdlock(&rwlock);
-        }
     }
-    pthread_rwlock_unlock(&rwlock);
-    time_to_exit = 1;
-    pthread_exit(0);
+    isexitall = true;
+    return nullptr;
 }
 
-void *thread_function_read_t(void *arg)
+void *thread_func_read_two(void *arg)
 {
-    printf("thread read two try to get lock\n");
-    pthread_rwlock_rdlock(&rwlock);
-    while (strncmp("end", work_area, 3) != 0)
+    std::cout << "read thread two start run." << std::endl;
+    while (strncmp("end", workarea, 3) != 0)
     {
-        printf("this is thread read two.");
-        printf("the characters is %s \n", work_area);
+        pthread_rwlock_lock(&rwlock);
+        std::cout << "read thread two get rwlock.";
+        std::cout << "workarea is :" << workarea << std::endl;
         pthread_rwlock_unlock(&rwlock);
         sleep(5);
-        pthread_rwlock_rdlock(&rwlock);
-        while (work_area[0] == '\0')
-        {
-            pthread_rwlock_unlock(&rwlock);
-            sleep(5);
-            pthread_rwlock_rdlock(&rwlock);
-        }
     }
-    pthread_rwlock_unlock(&rwlock);
-    time_to_exit = 1;
-    pthread_exit(0);
+    isexitall = true;
+    return nullptr;
 }
 
-void *thread_function_write_o(void *arg)
+void *thread_func_write_one(void *arg)
 {
-    printf("this is write thread one try to get lock\n");
-    while (!time_to_exit)
+    std::cout << "write thread one start run." << std::endl;
+    while (!isexitall)
     {
-        pthread_rwlock_wrlock(&rwlock);
-        printf("this is write thread one.\nInput some text. Enter 'end' to finish\n");
-        fgets(work_area, WORKSIZE, stdin);
+        pthread_rwlock_lock(&rwlock);
+        std::cout << "write thread one get rwlock.";
+        std::cout << "please input data:";
+        std::cin >> workarea;
         pthread_rwlock_unlock(&rwlock);
         sleep(15);
     }
-    pthread_rwlock_unlock(&rwlock);
-    pthread_exit(0);
+
+    return nullptr;
 }
 
-void *thread_function_write_t(void *arg)
+void *thread_func_write_two(void *arg)
 {
-    sleep(10);
-    while (!time_to_exit)
+    std::cout << "write thread two start run." << std::endl;
+    while (!isexitall)
     {
-        pthread_rwlock_wrlock(&rwlock); //获取写入锁
-        printf("this is write thread two.\nInput some text. Enter 'end' to finish\n");
-        fgets(work_area, WORKSIZE, stdin); //写入
-        pthread_rwlock_unlock(&rwlock);     //解锁
+        pthread_rwlock_lock(&rwlock);
+        std::cout << "write thread two get rwlock.";
+        std::cout << "please input data:";
+        std::cin >> workarea;
+        pthread_rwlock_unlock(&rwlock);
         sleep(20);
     }
-    pthread_rwlock_unlock(&rwlock); //解锁
-    pthread_exit(0);
+
+    return nullptr;
 }
---
+++ test/threadlocks/rwlock/readwritelock_tmp.cc
@@ -0,0 +1,169 @@
+
+/*****************************************************************************************
+ * @function    使用读写锁实现4个线程读写一段内存的实例。
+ * @notice      在任意时刻，如果有一个线程在写数据，将阻塞所有其他线程的任何操作。
+ * @author      xuchanglong
+ * @time        2019-09-17  
+ *****************************************************************************************/
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <bits/pthreadtypes.h>
+#include <unistd.h>
+#include <string.h>
+
+static pthread_rwlock_t rwlock; //读写锁对象
+
+#define WORKSIZE 1024
+char work_area[WORKSIZE];
+int time_to_exit;
+
+void *thread_function_read_o(void *arg);  //读线程1
+void *thread_function_read_t(void *arg);  //读线程2
+void *thread_function_write_o(void *arg); //写线程1
+void *thread_function_write_t(void *arg); //写线程2
+
+int main(int argc, char *argv[])
+{
+    int res;
+    pthread_t a_thread, b_thread, c_thread, d_thread;
+    void *thread_result;
+
+    res = pthread_rwlock_init(&rwlock, nullptr); //初始化读写锁
+    if (res != 0)
+    {
+        perror("rwlock initialization failed");
+        exit(EXIT_FAILURE);
+    }
+    res = pthread_create(&a_thread, NULL, thread_function_read_o, NULL); //create new thread创建线程
+    if (res != 0)
+    {
+        perror("Thread creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    res = pthread_create(&b_thread, NULL, thread_function_read_t, NULL); //create new thread
+    if (res != 0)
+    {
+        perror("Thread creation failed");
+        exit(EXIT_FAILURE);
+    }
+    res = pthread_create(&c_thread, NULL, thread_function_write_o, NULL); //create new thread
+    if (res != 0)
+    {
+        perror("Thread creation failed");
+        exit(EXIT_FAILURE);
+    }
+    res = pthread_create(&d_thread, NULL, thread_function_write_t, NULL); //create new thread
+    if (res != 0)
+    {
+        perror("Thread creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    res = pthread_join(a_thread, &thread_result); //等待a_thread线程结束
+    if (res != 0)
+    {
+        perror("Thread join failed");
+        exit(EXIT_FAILURE);
+    }
+    res = pthread_join(b_thread, &thread_result);
+    if (res != 0)
+    {
+        perror("Thread join failed");
+        exit(EXIT_FAILURE);
+    }
+    res = pthread_join(c_thread, &thread_result);
+    if (res != 0)
+    {
+        perror("Thread join failed");
+        exit(EXIT_FAILURE);
+    }
+    res = pthread_join(d_thread, &thread_result);
+    if (res != 0)
+    {
+        perror("Thread join failed");
+        exit(EXIT_FAILURE);
+    }
+
+    pthread_rwlock_destroy(&rwlock); //销毁读写锁
+    exit(EXIT_SUCCESS);
+}
+
+void *thread_function_read_o(void *arg)
+{
+    printf("thread read one try to get lock\n");
+
+    pthread_rwlock_rdlock(&rwlock); //获取读取锁
+    while (strncmp("end", work_area, 3) != 0)
+    {
+        printf("this is thread read one.");
+        printf("the characters is %s \n", work_area);
+        pthread_rwlock_unlock(&rwlock);
+        sleep(2);
+        pthread_rwlock_rdlock(&rwlock);
+        while (work_area[0] == '\0')
+        {
+            pthread_rwlock_unlock(&rwlock);
+            sleep(2);
+            pthread_rwlock_rdlock(&rwlock);
+        }
+    }
+    pthread_rwlock_unlock(&rwlock);
+    time_to_exit = 1;
+    pthread_exit(0);
+}
+
+void *thread_function_read_t(void *arg)
+{
+    printf("thread read two try to get lock\n");
+    pthread_rwlock_rdlock(&rwlock);
+    while (strncmp("end", work_area, 3) != 0)
+    {
+        printf("this is thread read two.");
+        printf("the characters is %s \n", work_area);
+        pthread_rwlock_unlock(&rwlock);
+        sleep(5);
+        pthread_rwlock_rdlock(&rwlock);
+        while (work_area[0] == '\0')
+        {
+            pthread_rwlock_unlock(&rwlock);
+            sleep(5);
+            pthread_rwlock_rdlock(&rwlock);
+        }
+    }
+    pthread_rwlock_unlock(&rwlock);
+    time_to_exit = 1;
+    pthread_exit(0);
+}
+
+void *thread_function_write_o(void *arg)
+{
+    printf("this is write thread one try to get lock\n");
+    while (!time_to_exit)
+    {
+        pthread_rwlock_wrlock(&rwlock);
+        printf("this is write thread one.\nInput some text. Enter 'end' to finish\n");
+        fgets(work_area, WORKSIZE, stdin);
+        pthread_rwlock_unlock(&rwlock);
+        sleep(15);
+    }
+    pthread_rwlock_unlock(&rwlock);
+    pthread_exit(0);
+}
+
+void *thread_function_write_t(void *arg)
+{
+    sleep(10);
+    while (!time_to_exit)
+    {
+        pthread_rwlock_wrlock(&rwlock); //获取写入锁
+        printf("this is write thread two.\nInput some text. Enter 'end' to finish\n");
+        fgets(work_area, WORKSIZE, stdin); //写入
+        pthread_rwlock_unlock(&rwlock);     //解锁
+        sleep(20);
+    }
+    pthread_rwlock_unlock(&rwlock); //解锁
+    pthread_exit(0);
+}
