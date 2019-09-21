ff0c1d943d909b69653b53266b92c68019bf2144
xuchanglong <wwwitworld@sina.com>
2019-09-21 21:29:51 +0800

    完善 socket 进程间通信的示例代码程序。

--- test/simple_tcp_udp_unix/unix/client.cc
+++ test/simple_tcp_udp_unix/unix/client.cc
@@ -1,37 +1,62 @@
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <unistd.h>
+#include <iostream>
+
 #define UNIX_DOMAIN "/tmp/UNIX.domain"
+
 int main(void)
 {
-    int connect_fd;
-    int ret;
-    char snd_buf[1024];
-    int i;
-    static struct sockaddr_un srv_addr;
-    //creat unix socket
-    connect_fd = socket(PF_UNIX, SOCK_STREAM, 0);
-    if (connect_fd < 0)
+    /**
+     * （1）定义相关变量。
+    */
+    int linkfd = 0;
+    int ret = 0;
+    char sendbuf[1024];
+    struct sockaddr_un addr_server;
+    int exitcode = 0;
+
+    /**
+     * （2）创建连接 socket 。
+    */
+    linkfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (linkfd < 0)
     {
-        perror("cannot create communication socket");
-        return 1;
+        std::cout << "cannot create communication socket" << std::endl;
+        exitcode = -1;
+        goto lblexit;
     }
-    srv_addr.sun_family = AF_UNIX;
-    strcpy(srv_addr.sun_path, UNIX_DOMAIN);
-    //connect server
-    ret = connect(connect_fd, (struct sockaddr *)&srv_addr, sizeof(srv_addr));
+
+    /**
+     * （3）设置 server 端 sockaddr_un 。
+    */
+    addr_server.sun_family = AF_UNIX;
+    memcpy(addr_server.sun_path, UNIX_DOMAIN, sizeof(UNIX_DOMAIN));
+
+    /**
+     * （4）连接 server 。 
+    */
+    ret = connect(linkfd, (struct sockaddr *)&addr_server, sizeof(addr_server));
     if (ret == -1)
     {
-        perror("cannot connect to the server");
-        close(connect_fd);
-        return 1;
+        std::cout << "cannot connect to the server" << std::endl;
+        exitcode = -2;
+        goto lblexit;
     }
-    memset(snd_buf, 0, 1024);
-    strcpy(snd_buf, "message from client");
-    //send info server
-    for (i = 0; i < 4; i++)
-        write(connect_fd, snd_buf, sizeof(snd_buf));
-    close(connect_fd);
-    return 0;
+    memset(sendbuf, 0, 1024);
+    strcpy(sendbuf, "message from client");
+
+    /**
+     * （5）向 server 发送数据。 
+    */
+    for (size_t i = 0; i < 4; i++)
+    {
+        write(linkfd, sendbuf, sizeof(sendbuf));
+    }
+
+lblexit:
+    close(linkfd);
+    unlink(UNIX_DOMAIN);
+    return exitcode;
 }
--- test/simple_tcp_udp_unix/unix/server.cc
+++ test/simple_tcp_udp_unix/unix/server.cc
@@ -3,70 +3,96 @@
  * https://www.jb51.net/article/127352.htm
  * https://blog.csdn.net/gladyoucame/article/details/8768731
 */
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <unistd.h>
+#include <iostream>
+
 #define UNIX_DOMAIN "/tmp/UNIX.domain"
+
 int main(void)
 {
-    socklen_t clt_addr_len;
-    int listen_fd;
-    int com_fd;
-    int ret;
-    int i;
+    /**
+     * （1）定义相关变量。
+    */
+    int listen_fd = 0;
+    int linkfd = 0;
     static char recv_buf[1024];
-    int len;
-    struct sockaddr_un clt_addr;
-    struct sockaddr_un srv_addr;
-    listen_fd = socket(PF_UNIX, SOCK_STREAM, 0);
+    socklen_t socklen = 0;
+    struct sockaddr_un addr_server;
+    struct sockaddr_un addr_client;
+    int ret = 0;
+    int exitcode = 0;
+
+    /**
+     * （2）创建连接 socket 。
+    */
+    listen_fd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (listen_fd < 0)
     {
-        perror("cannot create communication socket");
-        return 1;
+        std::cout << "cannot create communication socket" << std::endl;
+        exitcode = -1;
+        goto lblexit;
     }
-    //set server addr_param
-    srv_addr.sun_family = AF_UNIX;
-    strncpy(srv_addr.sun_path, UNIX_DOMAIN, sizeof(srv_addr.sun_path) - 1);
+
+    /**
+     * （3）设置 sockaddr_un 。
+    */
+    addr_server.sun_family = AF_UNIX;
+    strcpy(addr_server.sun_path, UNIX_DOMAIN);
+    /**
+     * 执行unlink()函数并不一定会真正的删除文件，它先会检查文件系统中此文件的连接数是否为1，
+     * 如果不是1说明此文件还有其他链接对象，因此只对此文件的连接数进行减1操作。
+     * 若连接数为1，并且在此时没有任何进程打开该文件，此内容才会真正地被删除掉。
+     * 在有进程打开此文件的情况下，则暂时不会删除，直到所有打开该文件的进程都结束时文件就会被删除。
+    */
     unlink(UNIX_DOMAIN);
-    //bind sockfd & addr
-    ret = bind(listen_fd, (struct sockaddr *)&srv_addr, sizeof(srv_addr));
+
+    /**
+     * （4）连接 socket 和 sockaddr_un 绑定。
+    */
+    ret = bind(listen_fd, (struct sockaddr *)&addr_server, sizeof(sockaddr_un));
     if (ret == -1)
     {
-        perror("cannot bind server socket");
-        close(listen_fd);
-        unlink(UNIX_DOMAIN);
-        return 1;
+        std::cout << "cannot bind server socket" << std::endl;
+        exitcode = -2;
+        goto lblexit;
     }
-    //listen sockfd
-    ret = listen(listen_fd, 1);
+
+    /**
+     * （5）将连接 socket 变为监听 socket 。
+    */
+    ret = listen(listen_fd, 5);
     if (ret == -1)
     {
-        perror("cannot listen the client connect request");
-        close(listen_fd);
-        unlink(UNIX_DOMAIN);
-        return 1;
+        std::cout << "cannot listen the client connect request" << std::endl;
+        exitcode = -3;
+        goto lblexit;
     }
-    //have connect request use accept
-    len = sizeof(clt_addr);
-    com_fd = accept(listen_fd, (struct sockaddr *)&clt_addr, &len);
-    if (com_fd < 0)
+
+    /**
+     * （6）开始接受 client 发来的连接。
+    */
+    socklen = sizeof(addr_client);
+    linkfd = accept(listen_fd, (struct sockaddr *)&addr_client, &socklen);
+    if (linkfd < 0)
     {
-        perror("cannot accept client connect request");
-        close(listen_fd);
-        unlink(UNIX_DOMAIN);
-        return 1;
+        std::cout << "cannot accept client connect request" << std::endl;
+        exitcode = -4;
+        goto lblexit;
     }
-    //read and printf sent client info
-    printf("/n=====info=====/n");
-    for (i = 0; i < 4; i++)
+
+    for (size_t i = 0; i < 4; i++)
     {
         memset(recv_buf, 0, 1024);
-        int num = read(com_fd, recv_buf, sizeof(recv_buf));
-        printf("Message from client (%d)) :%s/n", num, recv_buf);
+        int num = read(linkfd, recv_buf, sizeof(recv_buf));
+        std::cout << "Message from client is \"" << recv_buf << "\"" << std::endl;
     }
-    close(com_fd);
+
+lblexit:
+    close(linkfd);
     close(listen_fd);
     unlink(UNIX_DOMAIN);
-    return 0;
+    return exitcode;
 }
